d3a9d2c22b23c92f9cbec33e9cfcfd85bf3b2cdf
Yumi <ingyinmay1932003@gmail.com>
2026-05-01 12:54:24 +0630

    icon: define all icon in one folder

---
+++ frontend/src/icons/icon.ts
@@ -0,0 +1,12 @@
+export {
+  ArrowUp as ArrowUpIcon,
+  Moon as MoonIcon,
+  Eye as EyeIcon,
+  Pencil as PencilIcon,
+  Plus as PlusIcon,
+  Search as SearchIcon,
+  Sun as SunIcon,
+  Trash2 as TrashIcon,
+  Users as UsersIcon,
+  X as CloseIcon,
+} from "lucide-react";
